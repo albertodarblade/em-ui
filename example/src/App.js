26549310b449d26e894b5f2c68db737d10a74b7d
Alberto <alberto.blacutt@jalasoft.com>
2021-06-07 06:19:16 -0400

    minor fix

--- example/src/App.js
+++ example/src/App.js
@@ -1,40 +1,25 @@
 import React from 'react'
 
 import Em from 'em-ui'
+import 'em-ui/dist/index.css'
 
 const App = () => {
-  const [value, setValue] = React.useState(null);
-  const [options, setOptions] = React.useState([]);
-  const [loading, setLoading] = React.useState(false);
-
-  function handleChange(event, newValue) {
-    setValue(newValue)
-  }
-
-  async function getValues() {
-    setLoading(true);
-    const response = await fetch('https://restcountries.eu/rest/v2/all')
-    const data = await response.json();
-    setOptions(data);
-    setLoading(false)
-
-  }
-  return <Em.StyleProvider>
-      <Em.Button  color="warning" isPrimaryAction> Btton </Em.Button>
-      <Em.DateTimePicker readOnly language="es" />
-
-      <Em.Autocomplete
-        onOpen={getValues}
-        value={value}
-        onChange={handleChange}
-        loading={loading}
-        label="Epics"
-        options={options}
-        getOptionSelected={(option, value) => option.name === value.name}
-
+  return (
+    <Em.StyleProvider>
+      <Em.Login
+        head={
+          <img src='https://previews.123rf.com/images/alluranet/alluranet1712/alluranet171200004/91011424-educaci%C3%B3n-c-plantilla-de-dise%C3%B1o-del-logotipo-de-la-representaci%C3%B3n-.jpg' />
+        }
+        label='Sign in with your favorite account'
+        footer='terms and services'
+        facebookId='692152614907871'
+        googleId='876492811509-ji5aqh6i1chh7qg8hrgfg50sga5ni2ru.apps.googleusercontent.com'
+        buttonText='Sign in with'
+        onAuthenticate={(pay, response) => console.log(pay, response)}
+        size='large'
       />
-
     </Em.StyleProvider>
+  )
 }
 
 export default App
